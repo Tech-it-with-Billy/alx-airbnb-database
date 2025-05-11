0a3d9bcdbe6fb3014377a05c74b13bfec06fe6ac
BillyOch1999 <ochiengbilly02@gmail.com>
2025-05-11 09:01:27 +0300

    Initiating the database

---
+++ database-script-0x01/README.md
@@ -0,0 +1,13 @@
+# Alx Airbnb Database
+
+This repository contains the SQL schema for the **Alx Airbnb Database** backend project. The database is built using **PostgreSQL** and is designed to support features such as user management, property listings, bookings, reviews, and payments.
+
+## 📦 Database: `Alx Airbnb Database`
+
+### 🗂️ Core Tables
+- `users`: Stores guest and host account information.
+- `property`: Contains property details posted by hosts.
+- `bookings`: Records guest reservations.
+- `reviews`: Stores user feedback and ratings.
+- `payments`: Handles booking payment details.
+
---
+++ database-script-0x01/schema.sql
@@ -0,0 +1,62 @@
+CREATE DATABASE alx_airbnb_database;
+
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    phone VARCHAR(20),
+    is_host BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+
+CREATE TABLE property (
+    id SERIAL PRIMARY KEY,
+    host_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    address TEXT,
+    city VARCHAR(100),
+    state VARCHAR(100),
+    country VARCHAR(100),
+    price_per_night DECIMAL(10, 2) NOT NULL,
+    max_guests INTEGER,
+    num_bedrooms INTEGER,
+    num_bathrooms INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    property_id INTEGER REFERENCES property(id) ON DELETE CASCADE,
+    guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    check_in DATE NOT NULL,
+    check_out DATE NOT NULL,
+    total_price DECIMAL(10, 2),
+    status VARCHAR(50) DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE payments (
+    id SERIAL PRIMARY KEY,
+    booking_id INTEGER REFERENCES bookings(id) ON DELETE CASCADE,
+    amount DECIMAL(10, 2),
+    payment_method VARCHAR(100),
+    payment_status VARCHAR(50),
+    paid_at TIMESTAMP
+);
+
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    listing_id INTEGER REFERENCES listings(id) ON DELETE CASCADE,
+    reviewer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    rating INTEGER CHECK (rating BETWEEN 1 AND 5),
+    comment TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
