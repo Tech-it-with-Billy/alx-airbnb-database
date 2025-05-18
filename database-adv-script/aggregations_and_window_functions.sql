75c23521c3d5a5a96b214e7eea80bcf11d87ebbe
Billy Och <ochiengbilly02@gmail.com>
2025-05-18 11:04:32 +0300

    Updates

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,5 +4,14 @@ FROM(SELECT u.name, COUNT(b.*) number_of_bookings
     FROM bookings B
     INNER JOIN users u
     ON u.id = b.user_id
-    GROUP BY 1) t1
+    GROUP BY 1) t1;
 
+
+
+-- Rank properties based on the total number of bookings they have received.
+SELECT name, number_of_bookings, ROW_NUMBER() OVER (ORDER BY number_of_bookings DESC) AS booking_rank
+FROM(SELECT u.name, COUNT(b.*) number_of_bookings
+    FROM bookings B
+    INNER JOIN users u
+    ON u.id = b.user_id
+    GROUP BY 1) t2;
