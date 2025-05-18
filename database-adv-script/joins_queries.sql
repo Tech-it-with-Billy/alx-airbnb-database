dc361df2753b43f8286cc0ee1c02dc9a242d4fff
Billy Och <ochiengbilly02@gmail.com>
2025-05-18 10:04:20 +0300

    Add joins_queries file

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -6,10 +6,10 @@ ON b.user_id = u.id
 
 
 -- All properties and their reviews, including properties that have no reviews.
-SELECT p.*, r.*
+SELECT p.name, r.comment, r.rating
 FROM properties p
 LEFT JOIN reviews r
-ON p.id = r.property_id
+ON r.property_id = p.id
 
 
 
