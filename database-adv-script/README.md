c05b65621dc83ce909c5f2926b580a223af7b28b
Billy Och <ochiengbilly02@gmail.com>
2025-05-18 09:44:45 +0300

    Add README and joins_queries files

---
+++ database-adv-script/README.md
@@ -0,0 +1,4 @@
+The 3 scripts:-
+1. Retrieve all bookings and the respective users who made those bookings.
+2. Retrieve all properties and their reviews, including properties that have no reviews.
+3. Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,20 @@
+-- All bookings and the respective users who made those bookings.
+SELECT u.*, b.*
+FROM users u
+INNER JOIN bookings b
+ON b.user_id = u.id
+
+
+-- All properties and their reviews, including properties that have no reviews.
+SELECT p.*, r.*
+FROM property p
+LEFT JOIN reviews r
+ON r.property_id = p.id
+
+
+
+-- All users and all bookings, even if the user has no booking or a booking is not linked to a user.
+SELECT u.*, b.*
+FROM users u
+FULL OUTER JOIN bookings b
+ON b.user_id = u.id
