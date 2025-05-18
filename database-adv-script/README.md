b8f7a20d17a0c63fc74587ef3d3ef180415b38ca
Billy Och <ochiengbilly02@gmail.com>
2025-05-18 10:40:56 +0300

    Added subquries.sql and modified README.md file

--- database-adv-script/README.md
+++ database-adv-script/README.md
@@ -1,4 +1,9 @@
-The 3 scripts:-
+These scripts involve:
+####  Complex Queries with Joins
 1. Retrieve all bookings and the respective users who made those bookings.
 2. Retrieve all properties and their reviews, including properties that have no reviews.
 3. Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+
+####  Practicing Subqueries
+1. All properties where the average rating is greater than 4.0.
+2. Finding users who have made more than 3 bookings.
---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,18 @@
+-- All properties where the average rating is greater than 4.0.
+SELECT name, av_rating
+FROM(SELECT p.name, AVG(r.rating) av_rating
+    FROM properties p
+    INNER JOIN reviews r
+    ON r.property_id = p.id
+    GROUP BY 1) t1
+WHERE av_rating > 4;
+
+
+-- Users who have made more than 3 bookings.
+SELECT name, number_of_bookings
+FROM(SELECT u.name, COUNT(b.*) number_of_bookings
+    FROM bookings B
+    INNER JOIN users u
+    ON u.id = b.user_id
+    GROUP BY 1)
+WHERE number_of_bookings > 3
